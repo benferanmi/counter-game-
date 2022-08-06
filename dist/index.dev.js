9e6ded6b24a2a2c5c4dcbb7f49a03ea694ec38d1
benjamin <benferanmiopafunso@gmail.com>
2022-08-06 22:40:27 +0100

    first main branch

---
+++ dist/index.dev.js
@@ -0,0 +1,42 @@
+"use strict";
+
+saveOne = document.getElementById("saveone");
+saveTwo = document.getElementById("savetwo");
+scoresRightHand = document.getElementById("scoresright");
+scoresLeftHand = document.getElementById("scoresleft");
+var count = 0;
+
+function buttonone() {
+  count = count + 1;
+  saveOne.innerText = count;
+}
+
+function buttontwo() {
+  count = count + 1;
+  saveTwo.innerText = count;
+}
+
+function reset() {
+  count = 0;
+  saveOne.innerText = count;
+}
+
+var textt = 'Scores:' + "-";
+
+function resetscore() {
+  scoresRightHand.innerText = textt;
+}
+
+function save() {
+  score = count + "-";
+  scoresRightHand.innerText += score;
+  saveOne.innerText = 0;
+  count = 0;
+}
+
+function saveleft() {
+  score = count + "-";
+  scoresLeftHand.innerText += score;
+  saveTwo.innerText = 0;
+  count = 0;
+}
---
+++ index.js
@@ -0,0 +1,48 @@
+saveOne = document.getElementById("saveone")
+saveTwo = document.getElementById("savetwo")
+scoresRightHand = document.getElementById("scoresright")
+scoresLeftHand = document.getElementById("scoresleft")
+
+let count = 0
+
+function buttonone() {
+    count = count + 1
+    saveOne.innerText = count
+}
+
+function buttontwo() {
+    count = count + 1
+    saveTwo.innerText = count
+}
+
+function reset() {
+    count = 0
+    saveOne.innerText = count
+}
+function reset() {
+    count = 0
+    saveTwo.innerText = count
+}
+
+
+let textt = 'Scores:' + "-"
+function resetscore() {
+    scoresRightHand.innerText = textt
+}
+function resetscore() {
+    scoresLeftHand.innerText = textt
+}
+
+
+function save()  {
+    score = count + "-"
+    scoresRightHand.innerText += score
+    saveOne.innerText = 0
+    count = 0
+}
+function saveleft()  {
+    score = count + "-"
+    scoresLeftHand.innerText += score
+    saveTwo.innerText = 0
+    count = 0
+}
